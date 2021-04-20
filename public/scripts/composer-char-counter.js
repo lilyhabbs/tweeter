7f3d1590228d060f6d7357f7f4a6c6fa08aeefbd
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-20 18:49:39 +0000

    Implement jQuery to count and display characters of new tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#tweet-text').keyup(function(){
+    const maxLength = 140;
+    let currentLength = $(this).val().length;
+    const counter = $(this).closest('.new-tweet').find('.counter');
+
+    counter.html(maxLength - currentLength);
+    
+    if (currentLength > maxLength ){
+      counter.addClass('exceed-limit');
+    } else {
+      counter.removeClass('exceed-limit');
+    }
+  });
+});
