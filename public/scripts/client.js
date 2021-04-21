64b42baa6a2da6451ad52a9e54bebf4fe5070007
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-21 14:38:00 +0000

    Implement functions to dynamically render display of tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,65 @@
-// Display time since tweet was posted
 $(document).ready(function() {
-  timeago.render(document.querySelectorAll('.need_to_be_rendered'));
+  // Temporary data to be deleted
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  // Convert created date in epoch format to time ago format
+  const convertDate = function(epochDate) {
+    return timeago.format(epochDate);
+  };
+  
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+      <article class="tweet">
+      <header>
+      <span><img src="${tweet.user.avatars}">
+      ${tweet.user.name}</span>
+      <span>${tweet.user.handle}</span>
+      </header>
+      
+      <div>${tweet.content.text}</div>
+      
+      <footer>
+      <span>${convertDate(tweet.created_at)}</span>
+      <span>
+      <i class="icon fas fa-flag"></i>
+      <i class="icon fas fa-retweet"></i>
+      <i class="icon fas fa-heart"></i>
+      </span>
+      </footer>
+      </article>
+    `);
+    
+    return $tweet;
+  };
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  };
+  
+  renderTweets(data);
 });
