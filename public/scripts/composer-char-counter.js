5167003b439099ae65021576c00a11f45311818e
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-23 02:55:51 +0000

    Refactor and clean up for better readability

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,13 @@
 // Count the number of characters in a new tweet
 $(document).ready(function() {
-  $('#tweet-text').keyup(function(){
+  $('#tweet-text').keyup(function() {
     const maxLength = 140;
     let currentLength = $(this).val().length;
     const counter = $(this).closest('.new-tweet').find('.counter');
 
     counter.html(maxLength - currentLength);
     
-    if (currentLength > maxLength ){
+    if (currentLength > maxLength) {
       counter.addClass('exceed-limit');
     } else {
       counter.removeClass('exceed-limit');
